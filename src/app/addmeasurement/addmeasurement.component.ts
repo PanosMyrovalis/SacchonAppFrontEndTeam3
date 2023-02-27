b0e538b710a5e38af982c17d2ffca07864251d6b
Panos Myrovalis <pmyrovalis@athtech.gr>
2023-02-27 20:59:09 +0200

    Made some aesthetic changes

--- src/app/addmeasurement/addmeasurement.component.ts
+++ src/app/addmeasurement/addmeasurement.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { UserService } from '../service/user.service';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-addmeasurement',
   templateUrl: './addmeasurement.component.html',
   styleUrls: ['./addmeasurement.component.scss']
 })
-export class AddmeasurementComponent {
+export class AddmeasurementComponent implements OnInit {
+
+  response: any;
+
+  createForm: any;
+
+  // data = {
+  //   name: "maria",
+  //   job: "manager"
+  // }
+
+  constructor(private service: UserService,
+    private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    // this.service.post(this.data).subscribe({
+    //   next: data => this.response =  data
+    // })
+
+    this.createForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      phoneNumber: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      bloodType: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      creationDate: ['', [Validators.required]],
+    });
+  }
+
+  createMeasurement() {
+    // console.log(this.createForm.get('name'));
+    const data = {
+      glucose: this.createForm.get('glucose').value,
+      carb: this.createForm.get('carb').value,
+      measurement_date: this.createForm.get('measurement_date').value,
+    };
+
+    this.service.postMeasurement(data).subscribe({
+      next: res => this.response = res
+    })
+  }
+
+  get glucose(){return this.createForm.get('glucose');}
+  get carb(){return this.createForm.get('carb');}
+  get measurement_date(){return this.createForm.get('measurement_date');}
+
+
+}
+{
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -17,7 +17,7 @@ export class UserService {
 
 
   getPatient() {
-    return this.http.get('http://localhost:9000/Patient/2');
+    return this.http.get('http://localhost:9000/Patient/1');
   }
 
   getMeasurements() {
@@ -25,18 +25,18 @@ export class UserService {
   }
 
   getConsultations() {
-    return this.http.get('http://localhost:4200/profile');
+    return this.http.get('http://localhost:4200/Consultations/1');
   }
 
   getAverages() {
-    return this.http.get('http://localhost:4200/profile');
+    return this.http.get('http://localhost:9000/PatientAverageMeasurements/1');
   }
 
 
 
 
   postPatient(data: any) {
-    const url = 'http://http://localhost:9000/measurement';
+    const url = 'http://http://localhost:9000/PatientCreate';
 
     const headers = new HttpHeaders()
       .set('content-Type', 'application/json')
@@ -46,7 +46,7 @@ export class UserService {
   }
 
   postMeasurement(data: any) {
-    const url = 'http://http://localhost:9000/measurement';
+    const url = 'http://http://localhost:9000/StoreMeasurement';
 
     const headers = new HttpHeaders()
       .set('content-Type', 'application/json')
